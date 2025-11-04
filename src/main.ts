d09eb881173c8ac43c6a8a3994f0e0b31ac80013
Lucian miles <lucianmiles@ttr.group>
2025-11-04 11:58:15 +0700

    save code 01

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import '@fortawesome/fontawesome-free/css/all.css'
 import piniaPersistedState from 'pinia-plugin-persistedstate'
 import router from '@/routers'
-import './style.css'
-import App from './App.vue'
+import '@/style.css'
+import App from '@/App.vue'
 
 const app = createApp(App)
 const pinia = createPinia()
